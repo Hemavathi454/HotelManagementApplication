788340befd5bef9275a77c29227e141ad2fdf328
joycereshma110 <152403738+joycereshma110@users.noreply.github.com>
2026-04-15 13:58:56 +0530

    Add files via upload

---
+++ ReviewDTO.java
@@ -0,0 +1,63 @@
+package com.hotelmanagementapplication.hotel_management.DTOLayer;
+
+import java.time.LocalDate;
+
+public class ReviewDTO {
+	 private Long id;
+
+	    private Integer rating;
+	    private String comment;
+	    private LocalDate reviewDate;
+
+	    private Long reservationId;
+	    private Long userId;
+
+	    public Long getId() {
+	        return id;
+	    }
+
+	    public Integer getRating() {
+	        return rating;
+	    }
+
+	    public String getComment() {
+	        return comment;
+	    }
+
+	    public LocalDate getReviewDate() {
+	        return reviewDate;
+	    }
+
+	    public Long getReservationId() {
+	        return reservationId;
+	    }
+
+	    public Long getUserId() {
+	        return userId;
+	    }
+
+	    public void setId(Long id) {
+	        this.id = id;
+	    }
+
+	    public void setRating(Integer rating) {
+	        this.rating = rating;
+	    }
+
+	    public void setComment(String comment) {
+	        this.comment = comment;
+	    }
+
+	    public void setReviewDate(LocalDate reviewDate) {
+	        this.reviewDate = reviewDate;
+	    }
+
+	    public void setReservationId(Long reservationId) {
+	        this.reservationId = reservationId;
+	    }
+
+	    public void setUserId(Long userId) {
+	        this.userId = userId;
+	    }
+
+}
