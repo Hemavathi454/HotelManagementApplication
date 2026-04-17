5b825fc5a4b8f0cbc2c5c4dbcb31d18507233baf
joyce reshma <joycereshma3@gmail.com>
2026-04-17 08:26:03 +0530

    joyce commited the code

--- src/main/java/com/hotelmanagementapplication/hotel_management/ControllerLayer/AuthController.java
+++ src/main/java/com/hotelmanagementapplication/hotel_management/ControllerLayer/AuthController.java
@@ -1,6 +1,7 @@
 package com.hotelmanagementapplication.hotel_management.ControllerLayer;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +16,7 @@ import com.hotelmanagementapplication.hotel_management.ServiceLayer.AuthService;
 
 @RestController
 @RequestMapping("/auth")
+@CrossOrigin(origins = "http://localhost:52794")
 public class AuthController {
 
     @Autowired
---
+++ src/test/java/com/hotelmanagementapplication/hotel_management/ControllerTestLayer/HotelTestController.java
@@ -0,0 +1,5 @@
+package com.hotelmanagementapplication.hotel_management.ControllerTestLayer;
+
+public class HotelTestController {
+
+}
