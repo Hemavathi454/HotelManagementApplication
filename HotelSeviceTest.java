b61931b848447cf76f16a48fc6aa2db686bb7cd2
Hemavathi454 <hemavathivenkateswaran29@gmail.com>
2026-04-23 14:45:05 +0530

    Add files via upload
    hotelservice

--- HotelSeviceTest.java
+++ HotelSeviceTest.java
@@ -1,14 +1,15 @@
 package com.hotelmanagementapplication.hotel_management.ServiceTestLayer;
 
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.verify;
+
 import static org.mockito.Mockito.when;
 
-import java.util.Collections;
+
 import java.util.List;
 import java.util.Optional;
 
