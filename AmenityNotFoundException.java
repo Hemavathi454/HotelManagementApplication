8c6183ceb0bbe24620e144b32daec013d1dac6bd
Kaviya2608 <kaviyarajesh06@gmail.com>
2026-04-23 14:46:57 +0530

    amenity commit

---
+++ AmenityNotFoundException.java
@@ -0,0 +1,11 @@
+package com.hotelmanagementapplication.hotel_management.ExceptionLayer;
+
+
+	@SuppressWarnings("serial")
+	public class AmenityNotFoundException extends RuntimeException {
+	    public AmenityNotFoundException(String message) {
+	        super(message);
+	    }
+	}
+
+
