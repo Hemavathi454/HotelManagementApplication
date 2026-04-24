bab99e1d85cc1428bb42a8e0253c336dc8838578
Hemavathi454 <hemavathivenkateswaran29@gmail.com>
2026-04-24 11:34:30 +0530

    Add files via upload
    security configuration

---
+++ TestSecurityConfig.java
@@ -0,0 +1,28 @@
+package com.hotelmanagementapplication.hotel_management.CongifTestLayer;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+
+import com.hotelmanagementapplication.hotel_management.SecurityLayer.SecurityConfig;
+
+public class TestSecurityConfig {
+	
+
+	    private final SecurityConfig securityConfig = new SecurityConfig();
+
+	    @Test
+	    void testPasswordEncoderBean() {
+	        PasswordEncoder encoder = securityConfig.passwordEncoder();
+	        assertNotNull(encoder);
+	        String rawPassword = "secret123";
+	        String encoded = encoder.encode(rawPassword);
+	        //matches take raw password calculate hashes
+	        assertTrue(encoder.matches(rawPassword, encoded));
+	    }
+
+}
