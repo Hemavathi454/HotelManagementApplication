b02a2336e138899fa8e24d7beeb0804a9a965725
Hemavathi454 <hemavathivenkateswaran29@gmail.com>
2026-04-24 11:41:43 +0530

    Add files via upload
    hotel

---
+++ HotelRepository.java
@@ -0,0 +1,14 @@
+package com.hotelmanagementapplication.hotel_management.RepositoryLayer;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.hotelmanagementapplication.hotel_management.EntityClasses.Hotel;
+
+//INTERFACE CREATION
+public interface HotelRepository extends JpaRepository<Hotel, Long> {
+	 List<Hotel> findByLocation(String location);
+
+	    List<Hotel> findByNameContainingIgnoreCase(String name);
+}
