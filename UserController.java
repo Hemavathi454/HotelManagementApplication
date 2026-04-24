d12bd11ae42646917729f0b98a4b3fa945d9351d
Kaviya2608 <kaviyarajesh06@gmail.com>
2026-04-24 22:09:59 +0530

    user controller commit

---
+++ UserController.java
@@ -0,0 +1,63 @@
+package com.hotelmanagementapplication.hotel_management.ControllerLayer;
+
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.hotelmanagementapplication.hotel_management.DTOLayer.UserResponseDTO;
+
+import com.hotelmanagementapplication.hotel_management.RequesDTOLayer.UserRequestDTO;
+import com.hotelmanagementapplication.hotel_management.ServiceLayer.UserService;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    // CREATE USER
+    @PostMapping
+    public UserResponseDTO createUser(@Valid @RequestBody UserRequestDTO dto,
+                                      @RequestParam String password) {
+        return userService.createUser(dto, password);
+    }
+
+    // GET BY ID
+    @GetMapping("/{id}")
+    public UserResponseDTO getUser(@PathVariable Long id) {
+        return userService.getUserById(id);
+    }
+
+    // GET ALL
+    @GetMapping
+    public List<UserResponseDTO> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    // UPDATE
+    @PutMapping("/{id}")
+    public UserResponseDTO update(@PathVariable Long id,
+                                 @Valid @RequestBody UserRequestDTO dto) {
+        return userService.updateUser(id, dto);
+    }
+
+    // DELETE
+    @DeleteMapping("/{id}")
+    public String delete(@PathVariable Long id) {
+        userService.deleteUser(id);
+        return "User deleted successfully";
+    }
+}
